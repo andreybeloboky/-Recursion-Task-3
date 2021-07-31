5a67489617ce4c2bad9c5f83b8c16c51d1b4a9f1
kk5358972 <63634923+kk5358972@users.noreply.github.com>
2021-07-31 16:01:36 +0530

    Update Main.java
    Added a Method for Calculating Factorial

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,14 @@
 package com.company;
 
 public class Main {
+	
+    // Method for Calculating Factorial
+    public static int factorial(int userInput) {
+        if (userInput == 0)
+            return 1;
+        else
+            return userInput * factorial(userInput - 1);
+    }
 
     public static void main(String[] args) {
 	// write your code here
