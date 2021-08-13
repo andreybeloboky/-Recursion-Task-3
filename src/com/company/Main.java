4ebdea6807e8d257019b45572921f5f860f46e69
Andrew <87447398+andreybeloboky@users.noreply.github.com>
2021-08-13 14:35:32 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,6 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-	
-    // Method for Calculating Factorial
-    public static int factorial(int userInput) {
-        if (userInput == 0)
-            return 1;
-        else
-            return userInput * factorial(userInput - 1);
-    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
